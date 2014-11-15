9fd3ac7fdb3ebffeaa4be42f937e77995c9d2dbd
brantmerrell <brant.merrell@gmail.com>
2014-11-14 19:16:25 -0600

    mistakenly returns list

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,8 +1,21 @@
 pollutantmean<-function(directory, pollutant, id){
+  setwd("C:/Users/Josh/Documents/CSV")
   setwd(directory)
+  singlemean<-function(id){
   fac<-getElement(read.csv(getElement(list.files(), id)), pollutant)
-  mean(subset(fac, !is.na(fac)))
+  mean(subset(fac, !is.na(fac)))}
+  lapply(id, singlemean)
 }
-pollutantmean("specdata", "sulfate", 5)
-id<-5
-pollutant<-"sulfate"
+onetent<-pollutantmean("specdata", "sulfate", 1:10)
+lapply(onetent, mean)
+mean(c(3.880701,
+  4.460811,
+  4.327613,
+  4.214956,
+  4.210072,
+  4.102132,
+  3.820059,
+  4.781354,
+  3.645644,
+  0.6243649))
+[1] 3.806771
