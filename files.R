fdcd314381232c5455fc0c647f7d7dfdcc5a47d1
brantmerrell <brant.merrell@gmail.com>
2015-01-05 17:53:30 -0600

    transfer functions to notes.csv

--- files.R
+++
@@ -1,7 +0,0 @@
-#@"2014-10-15 06:30:45 CDT"
-file.append("001.csv", "002.csv")
-#   [1] TRUE
-
-#@"2014-10-15 06:27:17 CDT"
-file.exists("001.csv")
-#   [1] TRUE
--- weekdays.R
+++
@@ -1,8 +0,0 @@
-months(Sys.time())
-#   [1] "October"
-
-quarters(as.POSIXlt(Sys.time()))
-#   [1] "Q4"
-
-weekdays(Sys.Date())
-#   [1] "Wednesday"
