42504dac6dc4ecf2bdc32cb990819db8de8e5dc9
brantmerrell <brant.merrell@gmail.com>
2015-01-10 23:52:23 -0600

    remove some R functions

--- mapply.R
+++
@@ -1,9 +0,0 @@
-
-
-mapply(mean,as.numeric(care_outcome[,11]), na.rm=TRUE)
-##returns one number or NaN for each row
-
-mapply(mean, care_outcome, na.rm=TRUE)
-##returns one NA for each column
-
-mapply(FUN, ..., MoreArgs = NULL, SIMPLIFY = TRUE, USE.NAMES = TRUE)
--- names.R
+++
@@ -1,6 +0,0 @@
-names()
-
-"2014-10-15 14:23:48 CDT"
-names(by_package)
-# [1] "X"         "date"      "time"      "size"      "r_version" "r_arch"   
-# [7] "r_os"      "package"   "version"   "country"   "ip_id"  
