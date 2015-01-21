f11499525599150e4a8904b2c7d3b344654dba9d
brantmerrell <brant.merrell@gmail.com>
2015-01-20 21:41:35 -0600

    just keep trimming

--- remove.R
+++
@@ -1,5 +0,0 @@
-#@"2014-10-14 14:44:58 CDT"
-## swirl, Getting and Cleaning Data, Manipulating Data with dplyr
-rm("mydf")
-## successful
-# repeat @"2014-10-15 14:05:19 CDT"
--- row_colnames.R
+++
@@ -1,9 +0,0 @@
-
-colnames(read.csv("outcome-of-care-measures.csv"))
-
-## does schedule have rownames?
-rownames(schedule)
-#   returns 51 numbers
-
-?row.names
-?colnames
