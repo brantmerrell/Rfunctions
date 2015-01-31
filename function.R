b63a3f65978b5803040c9051cb932210eec1a24d
brantmerrell <brant.merrell@gmail.com>
2015-01-31 15:32:57 -0600

    split

--- function.R
+++ function.R
@@ -1,3 +1,12 @@
+
+completeID<-function(ID){
+  sample1<-sort(as.numeric(subset(chesspgn$move,chesspgn$ID==ID & chesspgn$color=="white")))
+  test1<-ifelse((length(sample1)!=0),identical(paste(sample1),paste(1:max(sample1))),FALSE)
+  sample2<-sort(as.numeric(subset(chesspgn$move,chesspgn$ID==ID & chesspgn$color=="black")))
+  test2<-ifelse((length(sample2)!=0),identical(paste(sample2),paste(1:max(sample2))),FALSE)
+  ifelse((test1==TRUE & test2==TRUE),return("complete"),return("incomplete"))
+}
+
 pgn.xy<-function(pgn){
   test<-function(sq){grepl(sq,pgn)}
   sq<-c(unlist(lapply(1:8,upboard)),"O")
