66743ac63a6343e8468b1377c401edfa1b53e8ec
brantmerrell <brant.merrell@gmail.com>
2015-01-31 22:58:29 -0600

    notate.pgn.R

---
+++ notate.pgn.R
@@ -0,0 +1,26 @@
+notate.pgn<-function(id){
+  pgn<-read.pgn(id)
+  pgn<-read.table(textConnection(pgn),sep=".",colClasses="character",comment.char="")
+  pgn<-matrix(pgn,nrow=ncol(pgn),ncol=1)
+  white<-function(n){
+    n<-n+1
+    as.character(read.table(textConnection(as.character(pgn[n,])),sep=" ")$V1)
+  }
+  black<-function(n){
+    n<-n+1
+    as.character(read.table(textConnection(as.character(pgn[n,])),sep=" ")$V2)
+  }
+  white<-unlist(lapply(1:(length(pgn)-1),white))
+  black<-unlist(lapply(1:(length(pgn)-1),black))
+  whitepaste<-function(n){
+    paste(n,white[n],sep=".")
+  }
+  blackpaste<-function(n){
+    paste(n,black[n],sep="...")
+  }
+  white<-unlist(lapply(1:length(pgn),whitepaste))
+  black<-unlist(lapply(1:length(pgn),blackpaste))
+  all<-sort(c(white,black))
+  all<-subset(all,grepl("NA",all)==FALSE)
+  return(all)
+}
