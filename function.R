6d6f5e40b7857c2e2b261c3388a6b822de08e712
brantmerrell <brant.merrell@gmail.com>
2015-03-21 18:27:39 +0000

    vecsample.R

--- function.R
+++ function.R
@@ -1,3 +1,64 @@
+list.files("C:/Users/Administrator/Documents/CSV")[(1:10)+(10*2)]
+vector<-as.character(read.csv("C:/Users/Administrator/Documents/CSV/gkg_PERSONS.csv")$x)
+vecsample<-function(vector,samplesize=5,add=0,method="letterparse"){
+  L<-length(vector)
+  if(class(vector)=="character" &
+       method=="letterparse"){
+    samplevec<-head(vector,samplesize)
+    #rm(l)
+    for(l in letters[1:26]){
+      lettersample<-vector[l==substr(vector,1,1)]
+      lettersample<-lettersample[
+        as.integer(length(lettersample)*c(1/(1:samplesize)))+add]
+      samplevec<-c(samplevec,lettersample)
+    }
+    #length(unique(printvec))
+    #length(printvec)
+    #subset(printvec,duplicated(printvec))
+    samplevec<-sort(unique(c(samplevec,tail(vector,samplesize))))
+  }
+  list(samplevec=samplevec,
+       arith=paste(L,"=",length(samplevec),"+",L-length(samplevec)),
+       ratio=paste(L,"=",length(samplevec),"*",as.integer(L/length(samplevec)))#,
+       #percentage=paste(round((length(samplevec)/L)*100,digits=2),"%",sep="")
+       )
+}
+
+update.PERSONS<-function(Date=Sys.Date()-1,project="gkg",command="return",
+                         workdir="C:/Users/Administrator/Documents"){
+  if(project=="gkg"){
+    df<-download.gdelt(Date,"gkg")
+    filepath<-file.path(workdir,"CSV","gkg_PERSONS.csv")
+  }
+  PRSNS<-levels(df$PERSONS)
+  rm(df)
+  PRSNS<-subset(PRSNS,PRSNS!="")
+  prsns<-subset(PRSNS,!grepl(";",PRSNS))
+  PRSNS<-subset(PRSNS,grepl(";",PRSNS))
+  workvec<-prsns
+  for(n in length(PRSNS)){
+    workvec<-unique(c(workvec,strsplit(PRSNS[n],";")[[1]]))
+  }
+  if(file.exists(filepath)){
+    workvec<-unique(c(workvec,read.csv(filepath,colClasses="character")))
+    print(filepath)
+  }
+  #write.csv(workvec,filepath,row.names=FALSE)
+  printvec<-c(head(workvec))
+  for(l in letters[1:26]){
+    lettersample<-workvec[l==substr(workvec,1,1)]
+    if(10<length(lettersample)){
+      lettersample<-lettersample[
+        as.integer(length(lettersample)*c(.1,.2,.3,.4,.5,.6,.7,.8,.9,1))]
+    }
+    printvec<-c(printvec,lettersample)
+  }
+  printvec<-unique(c(printvec,tail(workvec)))
+  write.csv(workvec,filepath)
+  print(printvec)
+}
+persons<-PERSONS()
+
 slice.lines<-function(lines){
   lines<-gsub("\"","\\\"",readLines(filepath))
   #lines[as.integer(length(lines)*c(.5,.1,.15,.2,.25,.3,.35,.4,.45,.50,.55,.6,.65,.7,.75,.8,.85,.9,.95,1))]
